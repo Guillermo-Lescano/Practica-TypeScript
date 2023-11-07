72407ae10ee32ec7cb4eef8179e17f3a7f3f6d21
Guillermo Lescano <guillelescano.gl@gmail.com>
2023-11-07 18:04:28 -0300

    Detalles en boton y estilos

--- Tareas-Pendientes-ReactTS/src/components/TodoApp.tsx
+++ Tareas-Pendientes-ReactTS/src/components/TodoApp.tsx
@@ -20,7 +20,7 @@ const TodoApp = () => {
   return (
     <div>
         <h1>Lista de Tareas</h1>
-        <div>
+        <div className='flex'>
             <input 
             type="text"
             value={newTasck}
